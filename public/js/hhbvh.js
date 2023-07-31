31c64a656295a1c108fdd6a5eae702a3fb9fbea6
Matias <rasmat23@gmail.com>
2023-07-31 20:14:11 -0300

    "agrege index y public, etc"

---
+++ public/js/hhbvh.js
@@ -0,0 +1,18 @@
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+
+
+ 
+
+
+
